448303171f37a16abead0653dedfceb1bd4ab031
ndm13 <ndm0113@gmail.com>
2018-04-10 12:58:40 -0400

    Add project files.

---
+++ Argsposer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace Argsposer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Argsposer v.1 - The Only Version You Need");
+            Console.WriteLine("─────────────────────────────────────────");
+            Console.WriteLine("Command Line Arguments:");
+            if (args.Length == 0)
+            {
+                Console.WriteLine(" [none]");
+            }
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine(" " + i + ": '" + args[i] + "'");
+                }
+            }
+            Console.WriteLine();
+            if (Console.IsInputRedirected)
+            {
+                Console.WriteLine("Piped Data:");
+                using (var input = Console.OpenStandardInput())
+                {
+                    using (var output = Console.OpenStandardOutput())
+                    {
+                        int bytes;
+                        byte[] data = new byte[2048];
+                        while ((bytes = input.Read(data, 0, data.Length)) > 0)
+                        {
+                            output.Write(data, 0, bytes);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No piped data.");
+            }
+            Console.WriteLine();
+            Console.Write("Ctrl-C to exit...");
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
